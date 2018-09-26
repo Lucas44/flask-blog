67ad4e15de100e6750d1b05415df2f76cef37a9a
Lucas44 <lucasmnelson2020@gmail.com>
2018-09-26 16:18:26 -0400

    html/css

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def intro_page():
-    return "Hello World!"
+    return render_template("index.html")
 
 @app.route("/newpage")
 def new_page():
@@ -11,3 +11,11 @@ def new_page():
 
 if __name__=='__main__':
     app.run(debug=True)
+
+@app.route("/home")
+def home_page():
+    return render_template("home.html")
+
+@app.route("/account")
+def account_page():
+    return render_template("account.html")
